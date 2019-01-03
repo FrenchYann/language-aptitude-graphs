85f0b14979eeb919246fe4c05ee3db92f7cd9f60
Unknown <yann.granjon@gmail.com>
2019-01-03 05:36:02 +0100

    nitpicking

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const root = d3.select('#root');
 
-makeExportButtons(root.node(), config);
+makeExportButtons(root.node(), window.config);
 update(root, window.config);
 make_gui(window.config, () => update(root, config));
